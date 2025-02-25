f46626bb97afaaed50038d884fe056012f2a5df8
ocueye2 <carsonmayn@fuse.net>
2025-02-25 15:43:36 -0500

    inproved

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request, send_from_directory, make_response, redirect
 import os
+import docker
+import git
+
+def makecontainerdir(dir,name,type):
+    os.makedirs(os.path.join(dir,name))
+    with open(os.path.join(dir,name,"subpath"),"w") as f:
+        f.write("""
+
+""")
+
+
+
+
 
 app = Flask(__name__, "/static","static")
 
 @app.route('/path', methods=['GET'])
 def render_path():
-    root = "/config/workspace/dockermanager"
+    root = "E:\\workspace\\dockertest\\dockermanager\\containers"
     subpath = request.args.get("path", root)  # Use request.args for GET requests
     mode = request.args.get("mode", "view")
     by = request.args.get("by","")
@@ -23,6 +36,7 @@ def render_path():
             for item in filelist:
                 fpath = os.path.join(fullpath, item)  # Correct path joining
                 if os.path.isdir(fpath):
+                    if os.path.isfile(os.path.join(fpath,"sublink") )
                     image = "icons/folder.png"
                 else:
                     image = "icons/file.png"
@@ -37,13 +51,18 @@ def render_path():
 
 @app.route('/new', methods=['GET'])
 def new():
+    location = request.args.get("path", None)
     match request.args.get("type", False):
         case False:
             responce = make_response(render_template("newfile.html"))
-            responce.set_cookie("dir",request.args.get("path", None))
+            responce.set_cookie("dir",location)
             return responce
         case "container":
-            return "wip"
+            match request.args.get("source", False):
+                case "git":
+                    return render_template("create/container/git.html")
+                case False:
+                    return render_template("create/container.html")
         case "stack":
             return "wip"
         case "folder":
@@ -58,7 +77,15 @@ def make():
             os.makedirs(request.cookies.get('dir') + "/" + request.args.get("filename", False))
 
             return redirect(f"path?path={request.cookies.get('dir')}")
-        
+        case "container":
+            match request.args.get("source", False):
+                case "git":
+                    os.makedirs(request.cookies.get('dir') + "/" + request.args.get("name", False))
+                    git.Repo.clone_from(request.args.get("repo"), request.cookies.get('dir') + "/" + request.args.get("name", False))
+
+                case "zip":
+                    print("zip")
+                    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8060, debug=True)
